779e6705a86801f3bd352b07e951aea819de5ac2
Dwarph <Dwarph@users.noreply.github.com>
2022-05-02 14:48:30 +0100

    Working portfolio generator

---
+++ js/portfolioGenerator.js
@@ -0,0 +1,62 @@
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', '../data/portfolioData.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(JSON.parse(xobj.responseText));
+        }
+    };
+    xobj.send(null);
+}
+
+loadJSON(function (items) {
+    console.log(items); // this will log out the json object
+
+    items.sort(function (a, b) {
+        return b.year - a.year ;
+    });
+
+    var years = [];
+
+    for (var i=0; i < items.length; i++) {
+        if (!years.includes(items[i].year)) {
+            years.push(items[i].year);
+
+            // add year item to DOM
+            document.getElementById('portfolio').innerHTML +=`
+            <div class="my-auto">
+            <div class="card card-year">
+              <div class="card-body d-flex align-items-center justify-content-center">
+                <p class="card-title"> ${items[i].year} </p>
+              </div>
+            </div>
+          </div>
+            `;
+        }
+
+        var cardMeta = items[i].year + ". ";
+        for(var j=0; j < items[i].tags.length; j++){
+            cardMeta += items[i].tags[j] + ", ";
+        }
+        cardMeta = cardMeta.slice(0, -2);
+
+        // add portfolio item to DOM
+        document.getElementById('portfolio').innerHTML += `
+        <div class="mt-5">
+        <div class="card card-portfolio">
+          <img class="card-img-top" src="../images/${items[i].image}" alt="${items[i].imageAlt}">
+          <div class="card-body">
+            <p class="card-title">${items[i].title}</p>
+            <p class="card-meta">${cardMeta}</p>
+            <p class="card-text">
+                ${items[i].description}
+            </p>
+          </div>
+        </div>
+      </div>
+        `;
+
+
+    }
+});
