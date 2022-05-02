f04687f38ad41003ca2c4147251365f0462c5771
Dwarph <Dwarph@users.noreply.github.com>
2022-05-02 22:06:51 +0100

    Add tag filters to portfolio page

--- js/portfolioGenerator.js
+++ js/portfolioGenerator.js
@@ -11,13 +11,12 @@ function loadJSON(callback) {
 }
 
 loadJSON(function (items) {
-    console.log(items); // this will log out the json object
-
     items.sort(function (a, b) {
         return b.year - a.year;
     });
 
     var years = [];
+    var tags = [];
 
     for (var i = 0; i < items.length; i++) {
         if (!years.includes(items[i].year)) {
@@ -28,7 +27,7 @@ loadJSON(function (items) {
             <div class="my-auto">
             <div class="card card-year">
               <div class="card-body d-flex align-items-center justify-content-center">
-                <p class="card-title"> ${items[i].year} </p>
+                <p class="card-title"> ${items[i].year}</p>
               </div>
             </div>
           </div>
@@ -36,14 +35,27 @@ loadJSON(function (items) {
         }
 
         var cardMeta = "";
+        var tagFilterButtons = "";
+        var tagList = "[";
         for (var j = 0; j < items[i].tags.length; j++) {
             cardMeta += items[i].tags[j] + ", ";
+            tagList += "\"" + items[i].tags[j] + "\", ";
+
+            if (!tags.includes(items[i].tags[j])) {
+                tags.push(items[i].tags[j]);
+            }
         }
+
+
         cardMeta = cardMeta.slice(0, -2);
+
+        tagList = tagList.slice(0, -1);
+        tagList += "\"";
+
         var links = "";
         if (items[i].links != undefined) {
             for (var j = 0; j < items[i].links.length; j++) {
-               links += `
+                links += `
                 <p class="card-text">
                 ${items[i].links[j].text} <a href=\"${items[i].links[j].link}\">here</a>
                 </p>
@@ -52,22 +64,56 @@ loadJSON(function (items) {
         }
         // add portfolio item to DOM
         document.getElementById('portfolio').innerHTML += `
-        <div class="mt-5">
-        <div class="card card-portfolio">
-        <img class="card-img-top" src="../images/${items[i].image}" alt="${items[i].imageAlt}">
-        <div class="card-body">
-        <p class="card-title">${items[i].title}</p>
-        <p class="card-meta">${cardMeta}</p>
-        <p class="card-text">
-        ${items[i].description}
-        </p>
-        ${links}
-        </div>
-        </div>
+        <div class="mt-5 card-portfolio-parent" data-tagList=${items[i].tags}>
+            <div class="card card-portfolio">
+                <img class="card-img-top" src="../images/${items[i].image}" alt="${items[i].imageAlt}">
+                <div class="card-body">
+                    <p class="card-title">${items[i].title}</p>
+                    <p class="card-meta">${cardMeta}</p>
+                    <p class="card-text">${items[i].description}</p>
+                    ${links}
+                </div> 
+            </div>
         </div>
         `;
-
-
-
     }
+
+    tags.sort();
+    for (var i = 0; i < tags.length; i++) {
+        tagFilterButtons += `
+        <input type=\"checkbox\" class=\"btn-check\" id=\"${tags[i]}\" autocomplete=\"off\" OnClick>
+        <label class=\"btn btn-outline-primary\" for=\"${tags[i]}\">${tags[i]}</label>
+        `
+    }
+    document.getElementById('tag-filters').innerHTML += `
+    ${tagFilterButtons}
+    `;
+
+    for (var i = 0; i < tags.length; i++) {
+        document.getElementById(tags[i]).addEventListener('change', function () {
+            var portfolioItems = document.getElementsByClassName("card-portfolio-parent");
+            if (this.checked) {
+                var inputs = document.getElementsByTagName("input");
+
+                for (var j = 0; j < inputs.length; j++) {
+                    if (inputs[j].type == "checkbox") {
+                        if (inputs[j].checked && inputs[j] != this) {
+                            inputs[j].checked = false;
+                        }
+                    }
+                }
+                for (var j = 0; j < portfolioItems.length; j++) {
+                    portfolioItems[j].removeAttribute("hidden");
+                    if (!portfolioItems[j].getAttribute("data-tagList").includes(this.id)) {
+                        portfolioItems[j].setAttribute("hidden", "");
+                    }
+                }
+            } else {
+                for (var j = 0; j < portfolioItems.length; j++) {
+                        portfolioItems[j].removeAttribute("hidden");
+                }
+            }
+        });
+    }
+
 });
